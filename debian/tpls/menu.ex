4bd792bcdd7397c20fa03d46da8cd4effafe629b
Thomas Ludwig <moonmaker@gmx.de>
2012-01-08 17:05:40 +0100

    latest version

--- debian/tpls/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(munin-vala-plugins):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="munin-vala-plugins" command="/usr/bin/munin-vala-plugins"
--- make_launchpad.sh
+++ make_launchpad.sh
@@ -4,6 +4,7 @@
 mkdir build
 cd build; cmake -DCMAKE_BUILD_TYPE:STRING="Release" -DCMAKE_VERBOSE_MAKEFILE=OFF -DCMAKE_INSTALL_PREFIX:PATH="/usr" -DBUILD_DOCUMENTATION=OFF -DCONFIG_CPACK="DEB" -DBUILD_INSTALLER=ON ..; make && ( cd .. )
 rm -R ../build
-git add .
+cd ..
+git add . -A
 git commit -m "latest version"
 git push
